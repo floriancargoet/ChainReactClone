f73d5800d06bb09709f7ab15f3c7cb569ddc46f1
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2012-10-31 18:40:37 +0100

    highscores

--- conf.lua
+++ conf.lua
@@ -2,5 +2,6 @@ function love.conf(t)
     t.screen.width  = 1280
     t.screen.height = 800
     t.title         = 'Chain Reaction'
+    t.identity      = 'chain-reaction'
     t.screen.vsync  = true
 end
--- states/gameover.lua
+++ states/gameover.lua
@@ -6,14 +6,22 @@ local print = loveutils.print
 local gameover = State:new()
 
 function gameover:init()
+    self.scoreFile = love.filesystem.newFile("highscores.txt")
 end
 
 function gameover:setScore(value)
     self.score = value
+    self:doHighScore()
 end
 
 function gameover:draw()
     print('Score: '..tostring(self.score))
+    print('')
+    print('Highscores')
+    for i, v in ipairs(self.highScores) do
+        print(tostring(i)..'. '..tostring(v))
+    end
+    print('')
     print('Press R to replay, Q to quit')
 end
 
@@ -27,6 +35,30 @@ end
 
 function gameover:replay()
     self:exit()
+end
+
+function gameover:doHighScore()
+    local f = self.scoreFile
+    local highScores = {
+        self.score
+    }
+    if love.filesystem.exists("highscores.txt") and f:open('r') then
+        for line in f:lines() do
+            table.insert(highScores, tonumber(line))
+        end
+        f:close()
+    end
+    table.sort(highScores, function(a, b) return a > b end)
+    while #highScores > 5 do
+        table.remove(highScores)
+    end
+    if f:open('w') then
+        for _, v in ipairs(highScores) do
+            f:write(tostring(v)..'\n')
+        end
+        f:close()
+    end
+    self.highScores = highScores
 end
 
 return gameover
